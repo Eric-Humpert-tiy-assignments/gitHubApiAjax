b5fc6766edd7d8528914ad919edfb34e1c6bc1fe
Eric Humpert <EricMac@ricks-mbp.apollosrc.root>
2016-05-05 14:59:59 -0500

    added user input functionality

--- public/scripts.js
+++ public/scripts.js
@@ -3,13 +3,26 @@ if (this.searchList === undefined) this.searchList = {};
 
 (function(context) {
 
+  var input = document.querySelector('#query-box');
+  var query = document.querySelector('#query-box').value;
+  var ul = document.querySelector('username-list');
+
   function start() {
-    
+
+    $.ajax('https://api.github.com/search/users?q=' + query.value).done(function(result) {
+      console.log('get result', result);
+      });
+
+    input.addEventListener('keyup', function(evt) {
+      if(evt.keyCode === 13) {
+        
+      }
+    })
     //Call your code here
     console.log('starting!', context);
-
+    console.log(query);
   }
 
-  window.searchList.start = start;
+  context.start = start;
 
 })(window.searchList);
